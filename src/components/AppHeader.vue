fe93df56f420902ca11986ea2155a07654af95ee
Andrew Westwick <awestwick@gmail.com>
2021-08-07 14:14:38 -0500

    add auth0

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,10 +2,31 @@
   <header>
     <div class="flex justify-between p-2">
       <div class="logo">logo</div>
-      <div class="login">login | register</div>
+      <div class="login" v-if="!$auth.loading">
+        <div v-if="$auth.isAuthenticated" class="flex">
+          <p class="mr-4">{{ $auth.user.email }}</p>
+          <button @click="logout">Log out</button>
+        </div>
+        <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      </div>
     </div>
   </header>
 </template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin,
+      });
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 header {
@@ -14,5 +35,9 @@ header {
 }
 .login {
   color: #3d4752;
+
+  button {
+    color: #ccc;
+  }
 }
 </style>
